6f7cdb197ae9aa74f7f28061b4f426931194c3f9
gabriel valomi <marques.valomi@gmail.com>
2026-04-23 22:03:09 -0300

    refinamento do back e criação das tabelas que estavam faltando

--- back/models.py
+++ back/models.py
@@ -1,16 +1,37 @@
 import sqlite3
 
 # Cria e conecta ao banco de dados local (arquivo será criado na mesma pasta do script)
-conn = sqlite3.connect('meu_banco.db')
+conn = sqlite3.connect('models.db')
 cursor = conn.cursor()
 
 # Cria tabela de usuários
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS usuarios (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     nome TEXT NOT NULL,
-    email TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
     senha TEXT NOT NULL
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS postagens (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    titulo TEXT NOT NULL,
+    conteudo TEXT NOT NULL,
+    autor_id INTEGER,
+    FOREIGN KEY (autor_id) REFERENCES usuarios (id)
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS comentarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    comentario TEXT NOT NULL,
+    postagem_id INTEGER,
+    autor_id INTEGER,
+    FOREIGN KEY (postagem_id) REFERENCES postagens (id),
+    FOREIGN KEY (autor_id) REFERENCES usuarios (id)
 )
 ''')
 
