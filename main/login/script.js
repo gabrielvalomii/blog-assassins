2ace6598d5fb1ed8b5efc0efb044ccef7c3b52c3
gabriel valomi <marques.valomi@gmail.com>
2026-04-25 14:30:03 -0300

    refinamento

--- main/login/script.js
+++ main/login/script.js
@@ -1,18 +1,22 @@
 document.querySelector('.login-form').addEventListener('submit', function(e) {
     e.preventDefault();
-    const nome = document.getElementById('email').value;
+    const email = document.getElementById('email').value;
     const senha = document.getElementById('senha').value;
     fetch('http://localhost:5000/api/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ nome, senha })
+        body: JSON.stringify({ email, senha })
     })
     .then(response => response.json())
     .then(data => {
         if (data.mensagem) {
             alert('login feito com sucesso!');
+            //salvar o usuário logado no localStorage
+            localStorage.setItem('usuario', JSON.stringify(data.usuario));
+            // Redireciona para a página principal
+            window.location.href = '../principal/index.html';
         } else {
             alert('Erro: ' + (data.erro || 'Não foi possível fazer login.'));
         }
